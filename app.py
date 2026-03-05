6811717bad2d62f2f11cd9276a5b65f218d7f1b5
Alicia Barradas Morales <L23020778@veracruz.tecnm.mx>
2026-03-04 22:50:35 -0600

    agregando ruta raiz

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route("/")
+def home():
+    return "Backend funcionando 🚀"
+
 conexion = mysql.connector.connect(
     host="crossover.proxy.rlwy.net",
     user="root",
